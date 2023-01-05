5f4c48a841ce2b254e6ab5d0d4bb6b4afaee421f
vikasvinayak <vikas000@gmail.com>
2023-01-04 16:35:55 -0800

    First

---
+++ forge.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  packagerConfig: {
+    'ignore':['node_modules','.gitignore','forge.config.js','.local_zsh_history']
+  },
+  hooks: {
+    postPackage: async (forgeConfig, options) => {
+      var fs = require('fs');
+      var filePath = 'out/my-app-darwin-arm64/my-app.app/Contents/Resources/app/package.json'; 
+      fs.unlinkSync(filePath);
+      console.log('Packages built at:');
+    },
+
+  },
+  rebuildConfig: {},
+  
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
